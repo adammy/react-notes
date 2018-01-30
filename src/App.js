008ad9ff5d6ed59f9f9929a5e1edafb400cf71cd
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-01-30 10:39:07 -0500

    Added some fake data that looks super real and whatnot

--- src/App.js
+++ src/App.js
@@ -24,15 +24,22 @@ class App extends Component {
 						{
 							id: v4(),
 							name: "Upcoming Projects",
-							content: "<p>This is some mock data.</p>",
-							datetime_updated: new Date('January 29, 2018 09:24:00'),
+							content: "<p><strong>Programming</strong></p><ul><li>Java OOP Bank Assignment (Feb 3)</li><li>Group Java Spring API Assignment (Feb 20)</li></ul><p><strong>User-Centered Design</strong></p><ul><li>Empathy Map Assignment (Feb 5)</li><li>User Personas Assignment (Feb 12)</li></ul><p><strong>Miscellaneous</strong></p><ul><li>Graduate School Essay</li></ul>",
+							datetime_updated: new Date('January 29, 2018 11:24:00'),
 							active: true
 						},
 						{
 							id: v4(),
+							name: "Graduate School Essay",
+							content: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><ul><li>Maecenas sed tellus eleifend magna.</li><li>Morbi quam ligula, dictum sed.</li><li>Mauris tincidunt augue vitae volutpat.</li></ul><p>Suspendisse urna ipsum, bibendum quis nunc ac, tempor tincidunt ipsum. Vestibulum eu iaculis dui, at porta orci. In ornare faucibus congue. Etiam in commodo lorem. Vestibulum volutpat, justo vitae viverra tincidunt, est nibh porta nulla.</p>",
+							datetime_updated: new Date('January 27, 2018 18:03:33'),
+							active: false
+						},
+						{
+							id: v4(),
 							name: "Class Schedule",
-							content: "<p>My super awesome schedule.</p>",
-							datetime_updated: new Date('December 10, 2017 03:24:00'),
+							content: "<p><strong>Monday</strong></p><p>10am–12pm / Programming / Room COP 1203</p><p><strong>Tuesday</strong></p><p>1pm–3:15pm / User-Centered Design / Room VAB 205</p><p><strong>Friday</strong></p><p>10am–12pm / Programming / Room COP 1203</p>",
+							datetime_updated: new Date('January 10, 2018 08:24:00'),
 							active: false
 						}
 					]
