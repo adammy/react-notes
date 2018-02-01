3ef212aebe01e336a2493817ea9b9071b9a858fe
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-01 17:17:59 -0500

    Add fontawesome as npm dependency; remove script call from public/html

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/fontawesome-free-solid';
 import './Nav.css';
 
 const Nav = ({ user }) => {
 
 	return (
 		<nav>
-			<h1>SimpleNote</h1>
+			<h1><FontAwesomeIcon icon={faBolt} /> FlashNote</h1>
 			<ul>
 				<li><a href="/profile">Hi, {user.name.first}</a></li>
 				<li><a href="/logout">Logout</a></li>
