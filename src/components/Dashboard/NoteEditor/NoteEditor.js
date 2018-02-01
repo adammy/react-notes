58dc8075659f0a432c117e4111c1777a7bdb04cc
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-01 16:43:19 -0500

    Converted NoteEditor into a stateless component

--- src/components/Dashboard/NoteEditor/NoteEditor.js
+++ src/components/Dashboard/NoteEditor/NoteEditor.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Editor, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './NoteEditor.css';
 
-class NoteEditor extends Component {
+const NoteEditor = ({ note, onNoteEditorChange = (f => f) }) => {
 
-	static propTypes = {
-		note: PropTypes.object,
-		onNoteEditorChange: PropTypes.func
-	};
-
-	handleKeyCommand(command, editorState) {
-		return this.onChange(RichUtils.handleKeyCommand(editorState, command));
-	}
-
-	render() {
-
-		const { note, onNoteEditorChange } = this.props;
-
-		return (
-			<div id="editor" className="panel">
-				<div className="title">{note.name}</div>
-				<div className="content">
-					<Editor
-						editorState={note.content}
-						handleKeyCommand={this.handleKeyCommand}
-						onChange={(editorState) => onNoteEditorChange(note.id, editorState)} />
-				</div>
+	return (
+		<div id="editor" className="panel">
+			<div className="title">{note.name}</div>
+			<div className="content">
+				<Editor
+					editorState={note.content}
+					handleKeyCommand={(command, editorState) => {
+						const newState = RichUtils.handleKeyCommand(editorState, command);
+						return onNoteEditorChange(note.id, newState);
+					}}
+					onChange={(editorState) => onNoteEditorChange(note.id, editorState)} />
 			</div>
-		);
+		</div>
+	);
 
-	}
+};
 
-}
+NoteEditor.propTypes = {
+	note: PropTypes.object,
+	onNoteEditorChange: PropTypes.func
+};
 
 export default NoteEditor;
