a4817ecd2820145e6b4c8317e611a587c876334a
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2018-02-02 10:07:42 -0500

    Remove fontawesome from nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faBolt } from '@fortawesome/fontawesome-free-solid';
 import './Nav.css';
 
 const Nav = ({ user }) => {
 
 	return (
 		<nav>
-			<h1><FontAwesomeIcon icon={faBolt} /> FlashNote</h1>
+			<h1>SimpleNote</h1>
 			<ul>
 				<li><a href="/profile">Hi, {user.name.first}</a></li>
 				<li><a href="/logout">Logout</a></li>
